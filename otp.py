8885d0be693f3fe99a550ef7baec5b1a3b085844
Nandini602 <107759330+Nandini602@users.noreply.github.com>
2022-10-16 22:23:00 +0530

    Add files via upload

---
+++ otp.py
@@ -0,0 +1,14 @@
+
+# generate the otp
+import random;
+import smtplib;
+
+server =smtplib.SMTP('smtp.gmail.com',587)
+# print(server)
+server.starttls()
+server.login('mendhenandini910@gmail.com',password= "tvsu gwhh hhcx jrdm")
+otp=''.join([str(random.randint(0,9))for i in range(4)])
+msg='Hello, your otp is '+str(otp)
+server.sendmail('mendhenandini910@gmail.com','mendhenandini44@gmail.com',msg)
+server.quit()
+print(otp)
